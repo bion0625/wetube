245a3f9c221ff22bca0ac99274970da0ffe80890
이의정 <stxtory@gmail.com>
2023-07-09 08:26:33 +0900

    export controller router

--- src/server.js
+++ src/server.js
@@ -1,32 +1,20 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./router/globalRouter";
+import userRouter from "./router/userRouter";
+import videoRouter from "./router/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
+
 app.use(logger);
 
-
-const globalRouter = express.Router();
-const handleHome = (req, res) => res.send("Home");
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-const handleEditUser = (req, res) => res.send("Edit User");
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-videoRouter.get("/watch", handleWatchVideo)
-
 app.use("/", globalRouter);
-app.use("/user", userRouter);
+app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
-
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
-
-
 
 app.listen(PORT, handleListening);
