f44b06e1f3dae35c517195effa973f2ef97a8ed8
이의정 <stxtory@gmail.com>
2023-07-08 23:29:42 +0900

    morgan

--- src/server.js
+++ src/server.js
@@ -1,41 +1,24 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
+
+const home = (req, res) => {
+    console.log("I will respond");
+    return res.send("hello");
 };
 
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if(url === "/protected"){
-        return res.send("<h1>Not Allowed</h1>");
-    }
-    console.log("Allowed, you may continue.")
-    next();
-};
-
-const handleHome = (req, res) => {
-    return res.send("<h1>I still love you</h1>");
-};
-
-const handleLogin = (req, res) => {
-    return res.send({message:"Login here"});
-};
-
-const handleProtected = (req, res) => {
-    return res.send("welcome to the private lounge!");
+const login = (req, res) => {
+    return res.send("login");
 };
 
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/login", handleLogin);
-app.get("/protected", handleProtected)
-
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
+app.get("/", home);
+app.get("/login", login);
 
 app.listen(PORT, handleListening);
