b8f6c0c52557c859740529864882004e84b2492d
이의정 <stxtory@gmail.com>
2023-07-09 21:10:27 +0900

    post request param

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -28,14 +28,19 @@ let videos = [
 export const trending = (req, res) => {
     return res.render("home", {pageTitle:"Home", videos})
 };
-export const search = (req, res) => res.send("Search");
 export const watch = (req, res) => {
     const { id } = req.params;
     const video = videos[id-1];
-    return res.render("watch", {pageTitle:`Watching ${video.title}`, video});
+    return res.render("watch", {pageTitle:`Watching: ${video.title}`, video});
 };
-export const edit = (req, res) => res.render("edit");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-    return res.send("deleteVideo");
+export const getEdit = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id-1];
+    return res.render("edit", {pageTitle:`Editing: ${video.title}`, video});
+};
+export const postEdit = (req, res) => {
+    const { title } = req.body;
+    const { id } = req.params;
+    videos[id-1].title = title;
+    return res.redirect(`/videos/${id}`);
 };
--- src/router/videoRouter.js
+++ src/router/videoRouter.js
@@ -1,11 +1,9 @@
 import express from "express";
-import { watch, edit, deleteVideo, upload } from "../controllers/videoController";
+import { watch, getEdit, postEdit } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
 videoRouter.get("/:id", watch);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.route("/:id/edit").get(getEdit).post(postEdit);
 
 export default videoRouter;
