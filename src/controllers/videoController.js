48a59aa5b68863d56345f8972d2e18a86c890bd8
이의정 <stxtory@gmail.com>
2023-07-09 10:53:08 +0900

    view html extends

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,7 @@
 export const trending = (req, res) => res.render("home");
 export const search = (req, res) => res.send("Search");
 export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => {
-    return res.send("Edit");
-};
+export const edit = (req, res) => res.render("edit");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
     return res.send("deleteVideo");
