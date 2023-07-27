5e0744f9d429c13163e9455f9dcc2e7eec17ed79
이의정 <stxtory@gmail.com>
2023-07-27 20:11:56 +0900

    video play timeline

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,7 @@ const muteBtn = document.getElementById('mute');
 const volumeRange = document.getElementById('volume');
 const currentTime = document.getElementById('currentTime');
 const totalTime = document.getElementById('totalTime');
+const timeline = document.getElementById('timeline');
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -52,15 +53,25 @@ const formatTime = (second) => {
 
 const handleLoadedMetaData = () => {
     totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max = Math.floor(video.duration);
     
 }
 
 const handleTimeUpdate = () => {
     currentTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);
 };
+
+const handleTimelineChange = (event) => {
+    const {
+        target:{ value },
+    } = event;
+    video.currentTime = value;
+}
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadedmetadata",handleLoadedMetaData);
 video.addEventListener("timeupdate",handleTimeUpdate);
+timeline.addEventListener("input", handleTimelineChange);
