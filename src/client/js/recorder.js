c2d05c37173a65461d68c62b78938509a946dc3e
이의정 <stxtory@gmail.com>
2023-07-29 21:13:54 +0900

    ffmpeg create

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,3 +1,5 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+
 const startBtn = document.getElementById('startBtn');
 const video = document.getElementById('preview');
 
@@ -5,7 +7,13 @@ let stream;
 let recorder;
 let videoFile;
 
-const handleDownload = () => {
+const handleDownload = async() => {
+    const ffmpeg = createFFmpeg({ log: true });
+    await ffmpeg.load();
+
+    ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+
+    await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
 
     const a = document.createElement("a");
     a.href = videoFile;
@@ -33,8 +41,6 @@ const handleStart = () => {
     recorder = new MediaRecorder(stream, {mimeType: "video/webm" });
     recorder.ondataavailable = (event) => {
         videoFile = URL.createObjectURL(event.data);
-        console.log(videoFile);
-
         video.srcObject = null;
         video.src = videoFile;
         video.loop = true;
--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,9 @@
 import multer from "multer";
 
 export const localsMiddleware = (req, res, next) => {
+    res.header("Cross-Origin-Embedder-Policy", "credentialless");
+    res.header("Cross-Origin-Opener-Policy", "same-origin");
+    
     res.locals.loggedIn = Boolean(req.session.loggedIn);
     res.locals.siteName = "Wetube";
     res.locals.loggedInUser = req.session.user || {};
