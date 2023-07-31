244d4336949745809c58cc1d48c45a0a042a0163
euijung.id <stxtory@gmail.com>
2023-07-31 20:19:33 +0900

    video recording code refactoring

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,32 +1,52 @@
 import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 
-const startBtn = document.getElementById('startBtn');
+const actionBtn = document.getElementById('actionBtn');
 const video = document.getElementById('preview');
 
 let stream;
 let recorder;
 let videoFile;
 
+const files = {
+    input: "recording.webm",
+    output: "output.mp4",
+    thumb: "thumbnail.jpg",
+};
+
+const downloadFile = (fileUrl, fileName) => {
+    const a = document.createElement("a");
+    a.href = fileUrl;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+};
+
 const handleDownload = async() => {
+    actionBtn.removeEventListener("click", handleDownload);
+
+    actionBtn.innerText = "Transcoding...";
+
+    actionBtn.disabled = true;
+
     const ffmpeg = createFFmpeg({ log: true });
     await ffmpeg.load();
 
-    ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+    ffmpeg.FS("writeFile", files.input, await fetchFile(videoFile));
 
-    await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+    await ffmpeg.run("-i", files.input, "-r", "60", files.output);
 
     await ffmpeg.run(
         "-i", 
-        "recording.webm", 
+        files.input, 
         "-ss", 
         "00:00:01", 
         "-frames:v", 
         "1", 
-        "thumbnail.jpg"
+        files.thumb
         );
 
-    const mp4File = ffmpeg.FS("readFile", "output.mp4");
-    const thumbnail = ffmpeg.FS("readFile", "thumbnail.jpg");
+    const mp4File = ffmpeg.FS("readFile", files.output);
+    const thumbnail = ffmpeg.FS("readFile", files.thumb);
 
     const mp4Blob = new Blob([mp4File.buffer], {type:"video/mp4"});
     const thumbnailBlob = new Blob([thumbnail.buffer], {type:"image/jpg"});
@@ -34,43 +54,35 @@ const handleDownload = async() => {
     const mp4Url = URL.createObjectURL(mp4Blob);
     const thumbnailUrl = URL.createObjectURL(thumbnailBlob);
 
-    const a = document.createElement("a");
-    a.href = mp4Url;
-    a.download = "MyRecording.mp4";
-    document.body.appendChild(a);
-    a.click();
+    downloadFile(mp4Url, "MyRecording.mp4");
 
-    const thumbnailA = document.createElement("a");
-    thumbnailA.href = thumbnailUrl;
-    thumbnailA.download = "MyThumbnail.jpg";
-    document.body.appendChild(thumbnailA);
-    thumbnailA.click();
+    downloadFile(thumbnailUrl, "MyThumbnail.jpg");
 
-    ffmpeg.FS("unlink", "recording.webm");
-    ffmpeg.FS("unlink", "output.mp4");
-    ffmpeg.FS("unlink", "thumbnail.jpg");
+    ffmpeg.FS("unlink", files.input);
+    ffmpeg.FS("unlink", files.output);
+    ffmpeg.FS("unlink", files.thumb);
 
     URL.revokeObjectURL(mp4Url);
     URL.revokeObjectURL(thumbnailUrl);
     URL.revokeObjectURL(videoFile);
-    
-    startBtn.innerText = "Start Recording";
-    startBtn.removeEventListener("click", handleDownload);
-    startBtn.addEventListener("click", handleStart);
+
+    actionBtn.disabled = false;
+    actionBtn.innerText = "Record Again";
+    actionBtn.addEventListener("click", handleStart);
     init();
-}
+};
 
 const handleStop = () => {
-    startBtn.innerText = "Downloading Recording";
-    startBtn.removeEventListener("click", handleStop);
-    startBtn.addEventListener("click", handleDownload);
+    actionBtn.innerText = "Downloading Recording";
+    actionBtn.removeEventListener("click", handleStop);
+    actionBtn.addEventListener("click", handleDownload);
     recorder.stop();
 };
 
 const handleStart = () => {
-    startBtn.innerText = "Stop Recording";
-    startBtn.removeEventListener("click", handleStart);
-    startBtn.addEventListener("click", handleStop);
+    actionBtn.innerText = "Stop Recording";
+    actionBtn.removeEventListener("click", handleStart);
+    actionBtn.addEventListener("click", handleStop);
 
     recorder = new MediaRecorder(stream, {mimeType: "video/webm" });
     recorder.ondataavailable = (event) => {
@@ -94,4 +106,4 @@ const init = async() => {
 
 init();
 
-startBtn.addEventListener("click", handleStart);
+actionBtn.addEventListener("click", handleStart);
